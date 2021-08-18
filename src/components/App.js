075ec3a9176bd10a4409cea06b1a4891d2eea4e1
Supriya Maji <78439168+supriyamaji1998@users.noreply.github.com>
2021-08-18 21:20:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,33 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+function getTime() {
+  const current = new Date();
+  return current.toLocaleTimeString().toUpperCase();
 }
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: getTime(),
+    };
+  }
+  componentDidMount() {
+    this.clk = setInterval(() => {
+      this.setState({ time: getTime() });
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.clk);
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.state.time}</h3>
+      </div>
+    );
+  }
+}
 
 export default App;
